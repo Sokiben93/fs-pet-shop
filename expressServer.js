3fcf220917dccce3b47fab59ba11550e1099c698
Thanh Huy Le <thanhhuyle@Thanhs-MBP.lan>
2022-07-15 20:38:08 -0500

    Update express server part 1, 2

---
+++ expressServer.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var fs = require('fs'); // Setup fs and path
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
+var express = require('express'); // Setup express and app
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.disable('x-powered-by');
+// GET METHOD
+app.get('/pets', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
+});
+// GET with /pets/:id
+app.get('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(404);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'application/json').send(pets[id]);
+  });
+});
+// Error handle
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+// Listening port
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
